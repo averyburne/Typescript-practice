1d422d8a3400c18b0f04a47c73183ef51c86aff1
Avery Burne <burneav@gmail.com>
2022-07-01 13:54:58 -0400

    Added form in HTML

--- script.ts
+++ script.ts
@@ -2,7 +2,11 @@ export {}
 
 let nameHTML: any = document.getElementById("name");
 let btnGet = document.getElementById('btn-get')
-let nameVal: string = nameHTML.value
+let nameVal: number = nameHTML.value
+let firstNameHTML: any = document.getElementById("first-name");
+let lastNameHTML: any = document.getElementById("last-name");
+let weightHTML: any = document.getElementById("weight");
+
 
 btnGet.addEventListener('click', () => {
     document.getElementById("box-text").innerText = nameHTML.value
