9412ebdffe2e2a971269c45eb173fbdb22e3f049
Avery Burne <burneav@gmail.com>
2022-07-06 12:43:10 -0400

    Got BMI calculation to work correctly

--- script.js
+++ script.js
@@ -3,6 +3,28 @@ exports.__esModule = true;
 var nameHTML = document.getElementById("name");
 var btnGet = document.getElementById('btn-get');
 var nameVal = nameHTML.value;
+var heightHTML = document.getElementById("height").value;
+var weightHTML = document.getElementById("weight");
+// const convertToMeterSquared = (height: number, units: string): number => {
+//     if (units === 'inches') {
+//         return (height * 0.3048) ** 2
+//     }
+//     if 
+// }
+// setTimeout( () => {
+//     console.log(heightHTML)
+// }, 3000)
+var calculateBMI = function () {
+    console.log("Hi");
+    var heightHTML = document.getElementById("height").value;
+    var weightHTML = document.getElementById("weight").value;
+    return weightHTML / (Math.pow(heightHTML, 2));
+};
+var displayBMI = function () {
+    console.log(calculateBMI());
+    // console.log(weightHTML)
+    document.getElementById("BMI-display").innerText = calculateBMI().toString();
+};
 btnGet.addEventListener('click', function () {
     document.getElementById("box-text").innerText = nameHTML.value;
     console.log(nameVal);
--- script.ts
+++ script.ts
@@ -3,12 +3,33 @@ export {}
 let nameHTML: any = document.getElementById("name");
 let btnGet = document.getElementById('btn-get')
 let nameVal: number = nameHTML.value
-let firstNameHTML: any = document.getElementById("first-name");
-let lastNameHTML: any = document.getElementById("last-name");
-let weightHTML: any = document.getElementById("weight");
+let heightHTML: any = (<HTMLInputElement>document.getElementById("height")).value;
+let weightHTML: any = (<HTMLInputElement>document.getElementById("weight"))
 
+// const convertToMeterSquared = (height: number, units: string): number => {
+//     if (units === 'inches') {
+//         return (height * 0.3048) ** 2
+//     }
+//     if 
+// }
+// setTimeout( () => {
+//     console.log(heightHTML)
+// }, 3000)
+const calculateBMI = (): number => {
+    console.log("Hi")
+    let heightHTML: any = (<HTMLInputElement>document.getElementById("height")).value
+    let weightHTML: any = (<HTMLInputElement>document.getElementById("weight")).value
+    return weightHTML/(heightHTML**2)
+}
+
+const displayBMI = ():void => {
+    console.log(calculateBMI())
+    // console.log(weightHTML)
+    document.getElementById("BMI-display").innerText = calculateBMI().toString();
+}
 
 btnGet.addEventListener('click', () => {
     document.getElementById("box-text").innerText = nameHTML.value
     console.log(nameVal)
 })
+
