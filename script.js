6d598283ad78ec8dd71fa6810d6f59f8c7d2d118
Avery Burne <burneav@gmail.com>
2022-07-06 16:52:15 -0400

    Added rounding for BMI

--- script.js
+++ script.js
@@ -18,7 +18,11 @@ var calculateBMI = function () {
     console.log("Hi");
     var heightHTML = document.getElementById("height").value;
     var weightHTML = document.getElementById("weight").value;
-    return weightHTML / (Math.pow(heightHTML, 2));
+    var heightInMeters = heightHTML * 0.0254;
+    var weightInKg = weightHTML * 0.453592;
+    var BMI = weightInKg / (Math.pow(heightInMeters, 2));
+    return Math.round(BMI * 100) / 100;
+    console.log(Math.round(4 * 100) / 100);
 };
 var displayBMI = function () {
     console.log(calculateBMI());
--- script.ts
+++ script.ts
@@ -15,11 +15,18 @@ let weightHTML: any = (<HTMLInputElement>document.getElementById("weight"))
 // setTimeout( () => {
 //     console.log(heightHTML)
 // }, 3000)
+
+
 const calculateBMI = (): number => {
     console.log("Hi")
     let heightHTML: any = (<HTMLInputElement>document.getElementById("height")).value
     let weightHTML: any = (<HTMLInputElement>document.getElementById("weight")).value
-    return weightHTML/(heightHTML**2)
+    let heightInMeters: number = heightHTML*0.0254
+    let weightInKg: number = weightHTML*0.453592
+    let BMI = weightInKg/(heightInMeters**2)
+    return Math.round(BMI * 100) / 100
+
+    console.log(Math.round(4 * 100)/100)
 }
 
 const displayBMI = ():void => {
